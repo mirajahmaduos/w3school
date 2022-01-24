273c229af7ad02eac5d576c3b20570bfa13d9855
mirajahmaduos <mirajahmaduae@gmail.com>
2022-01-24 22:25:08 +0500

    commit 1

---
+++ demo_uppercase.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var uc = require('upper-case');
+
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write(uc.upperCase("Hello World this text wil be written in upper case"));
+    res.end();
+}).listen(8080);
+
+
